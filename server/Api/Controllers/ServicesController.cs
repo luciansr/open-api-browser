73ec1008ef0fe88cfaf7fa53861b6a745c1078f7
Lucian Sturião <luciansr@gmail.com>
2019-06-20 12:37:54 -0300

    Create initial API definition

---
+++ server/Api/Controllers/ServicesController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetAll()
+        {
+            return Ok(new[] { "value1", "value2" });
+        }
+        
+        [HttpGet("{workspace}")]
+        public async Task<ActionResult<IEnumerable<string>>> GetWorkspace()
+        {
+            return Ok(new[] { "value1", "value2" });
+        }
+
+        // GET api/values/5
+        [HttpGet("{workspace}/{service}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            return Ok("value");
+        }
+
+        // POST api/values
+        [HttpPost("{workspace}/{service}")]
+        public async Task Post([FromBody] string value)
+        {
+        }
+    }
+}
--- server/Api/Startup.cs
+++ server/Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Models.Config;
 
 namespace Api
 {
@@ -26,6 +27,16 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            
+            BindSectionToConfigObject<AwsConfig>(Configuration, services);
+        }
+        
+        private static void BindSectionToConfigObject<TType>(IConfiguration configuration, IServiceCollection services)
+            where TType : class, new()
+        {
+            var typeConfig = new TType();
+            configuration.Bind(typeConfig.GetType().Name, typeConfig);
+            services.AddSingleton(typeConfig);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
